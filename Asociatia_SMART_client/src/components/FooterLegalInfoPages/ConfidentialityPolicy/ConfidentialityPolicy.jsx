13abb4a73b02ac77582b7c44aa4f6ee8ddadcacc
Bobea Dorin-Adrian <bobeadorin@yahoo.com>
2024-03-07 13:02:08 +0200

    Finished Terms and Policy styles

--- Asociatia_SMART_client/src/components/FooterLegalInfoPages/ConfidentialityPolicy/ConfidentialityPolicy.jsx
+++ Asociatia_SMART_client/src/components/FooterLegalInfoPages/ConfidentialityPolicy/ConfidentialityPolicy.jsx
@@ -1,5 +1,115 @@
-import "./ConfidentialityPolicyStyles.css";
+import "../TermsAndConditions/TermsAndConditionsStyles.css";
 
 export default function ConfidentialityPolicy() {
-  return <div>..</div>;
+  return (
+    <article className="termsAndConditions-container">
+      <section className="terms-content-wrapper">
+        <h1 className="terms-section-title">
+          Mulțumim pentru că ați ales să vă conectați cu Asociația SMART.
+        </h1>
+        <p className="terms-starting-p">
+          Această politică de confidențialitate explică cum colectăm, utilizăm,
+          protejăm și dezvăluim informațiile dvs. în timpul utilizării site-ului
+          nostru și a serviciilor asociate.
+        </p>
+
+        <h2 class="terms-titles">1. Informații Colectate</h2>
+        <p class="terms-subTitles">
+          <strong>Informații Personale:</strong>
+        </p>
+        <ul class="terms-text-wrapper">
+          <li class="terms-normalText">Numele și prenumele</li>
+          <li class="terms-normalText">Adresa de e-mail</li>
+          <li class="terms-normalText">Numărul de telefon</li>
+          <li class="terms-normalText">
+            Alte informații pe care le furnizați voluntar prin formulare sau
+            corespondență.
+          </li>
+        </ul>
+        <p class="terms-subTitles">
+          <strong>Informații de Utilizare:</strong>
+        </p>
+        <ul class="terms-text-wrapper">
+          <li class="terms-normalText">Adresa IP</li>
+          <li class="terms-normalText">Dispozitivul și browser-ul utilizat</li>
+          <li class="terms-normalText">Paginile vizitate pe site-ul nostru</li>
+        </ul>
+
+        <h2 class="terms-titles">2. Scopul Colectării Informațiilor</h2>
+        <p class="terms-text-wrapper">
+          Informațiile colectate sunt utilizate în principal pentru:
+        </p>
+        <ul class="terms-text-wrapper">
+          <li class="terms-normalText">
+            Contactarea și răspunsul la solicitările dvs.
+          </li>
+          <li class="terms-normalText">
+            Îmbunătățirea și personalizarea experienței dvs. pe site.
+          </li>
+          <li class="terms-normalText">
+            Furnizarea de informații despre evenimente, programe și campanii
+            relevante.
+          </li>
+          <li class="terms-normalText">
+            Analiza și îmbunătățirea serviciilor noastre.
+          </li>
+        </ul>
+
+        <h2 class="terms-titles">3. Păstrarea și Securitatea Informațiilor</h2>
+        <p class="terms-text-wrapper">
+          Informațiile dvs. personale sunt păstrate în condiții de siguranță și
+          confidențialitate. Implementăm măsuri tehnice și organizatorice pentru
+          a proteja împotriva accesului neautorizat sau a utilizării incorecte.
+        </p>
+
+        <h2 class="terms-titles">4. Partajarea Informațiilor</h2>
+        <p class="terms-text-wrapper">
+          Nu vom partaja, vinde sau închiria informațiile dvs. personale
+          terțelor părți fără consimțământul dvs., cu excepția cazurilor în care
+          acest lucru este necesar în conformitate cu legea.
+        </p>
+
+        <h2 class="terms-titles">
+          5. Drepturile Dvs. cu Privire la Informații
+        </h2>
+        <p class="terms-text-wrapper">
+          Aveți dreptul să solicitați acces, corectare, ștergere sau
+          restricționarea utilizării informațiilor dvs. personale. Ne puteți
+          contacta pentru a exercita aceste drepturi.
+        </p>
+
+        <h2 class="terms-titles">6. Cookie-uri și Tehnologii Similare</h2>
+        <p class="terms-text-wrapper">
+          Site-ul nostru utilizează cookie-uri și tehnologii similare pentru a
+          îmbunătăți funcționalitatea și experiența utilizatorului. Prin
+          utilizarea site-ului, sunteți de acord cu utilizarea acestor
+          tehnologii.
+        </p>
+
+        <h2 class="terms-titles">
+          7. Modificări la Politica de Confidențialitate
+        </h2>
+        <p class="terms-text-wrapper">
+          Ne rezervăm dreptul de a modifica această politică de
+          confidențialitate. Orice modificări vor fi publicate pe această
+          pagină.
+        </p>
+
+        <h2 class="terms-titles">8. Contact</h2>
+        <p class="terms-text-wrapper">
+          Dacă aveți întrebări sau preocupări cu privire la această politică de
+          confidențialitate, vă rugăm să ne contactați la adresa{" "}
+          <a href="mailto:contact.asociatiasmart@gmail.com">
+            contact.asociatiasmart@gmail.com
+          </a>
+          .
+        </p>
+
+        <p class="terms-text-wrapper">
+          Prin utilizarea site-ului nostru, sunteți de acord cu această politică
+          de confidențialitate.
+        </p>
+      </section>
+    </article>
+  );
 }
--- Asociatia_SMART_client/src/components/FooterLegalInfoPages/TermsAndConditions/TermsAndConditions.jsx
+++ Asociatia_SMART_client/src/components/FooterLegalInfoPages/TermsAndConditions/TermsAndConditions.jsx
@@ -1,5 +1,147 @@
 import "./TermsAndConditionsStyles.css";
+import TextBlock from "./TermsAndConditionsComponents/TextBlock";
 
-export default function TermsAndConditions() {
-  return <div>TermsAndConditions</div>;
+export default function TermsAndConditions(props) {
+  return (
+    <article className="termsAndConditions-container">
+      <section className="terms-content-wrapper">
+        <h1 className="terms-section-title">
+          Bun venit pe site-ul și în comunitatea Asociației SMART.
+        </h1>
+        <p className="terms-starting-p">
+          Prin accesarea și utilizarea serviciilor noastre, sunteți de acord să
+          respectați acești Termeni și Condiții. Vă rugăm să citiți cu atenție
+          aceste informații.
+        </p>
+
+        <h2 className="terms-titles">1. Utilizarea Site-ului</h2>
+        <p>
+          <strong className="terms-subTitles">
+            1.1. Accesul și Utilizarea:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Prin accesarea și utilizarea site-ului nostru, vă angajați să
+            respectați acești Termeni și Condiții.
+          </li>
+          <li className="terms-normalText">
+            Sunteți responsabil pentru furnizarea de informații corecte și
+            actualizate în timpul utilizării serviciilor noastre.
+          </li>
+        </ul>
+        <p>
+          <strong className="terms-subTitles">
+            1.2. Utilizarea Necorespunzătoare:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Nu sunteți permis să utilizați site-ul în scopuri ilegale sau
+            neetice.
+          </li>
+          <li className="terms-normalText">
+            Orice utilizare abuzivă a site-ului poate duce la suspendarea sau
+            încetarea accesului dvs.
+          </li>
+        </ul>
+
+        <h2 className="terms-titles">2. Informații Personale</h2>
+        <p>
+          <strong className="terms-subTitles">2.1. Confidențialitate:</strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Informațiile personale furnizate vor fi tratate conform Politicii de
+            Confidențialitate a Asociației SMART.
+          </li>
+        </ul>
+        <p>
+          <strong className="terms-subTitles">
+            2.2. Corectitudinea Informațiilor:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Sunteți responsabil pentru corectitudinea informațiilor personale
+            furnizate și pentru menținerea acestora actualizate.
+          </li>
+        </ul>
+
+        <h2 className="terms-titles">3. Conținutul Site-ului</h2>
+        <p>
+          <strong className="terms-subTitles">
+            3.1. Drepturi de Proprietate Intelectuală:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Conținutul site-ului, inclusiv textele, imaginile și alte materiale,
+            sunt protejate de drepturile de autor și alte legi privind
+            proprietatea intelectuală.
+          </li>
+        </ul>
+        <p>
+          <strong className="terms-subTitles">
+            3.2. Utilizarea Conținutului:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Nu aveți voie să reproduceți, distribuiți sau modificați conținutul
+            site-ului fără permisiunea expresă a Asociației SMART.
+          </li>
+        </ul>
+
+        <h2 className="terms-titles">4. Responsabilitate</h2>
+        <p>
+          <strong className="terms-subTitles">
+            4.1. Exonerare de Răspundere:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Asociația SMART nu poate fi trasă la răspundere pentru prejudiciile
+            sau pierderile survenite din utilizarea sau incapacitatea de
+            utilizare a site-ului.
+          </li>
+        </ul>
+
+        <h2 className="terms-titles">
+          5. Modificări ale Termenilor și Condițiilor
+        </h2>
+        <p>
+          <strong className="terms-subTitles">5.1. Actualizări:</strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Asociația SMART își rezervă dreptul de a modifica acești Termeni și
+            Condiții în orice moment. Orice modificare va fi afișată pe site.
+          </li>
+        </ul>
+
+        <h2 className="terms-titles">6. Contact</h2>
+        <p>
+          <strong className="terms-subTitles">
+            6.1. Întrebări și Comentarii:
+          </strong>
+        </p>
+        <ul className="terms-text-wrapper">
+          <li className="terms-normalText">
+            Pentru întrebări sau comentarii cu privire la acești Termeni și
+            Condiții, vă rugăm să ne contactați la adresa{" "}
+            <a href="mailto:contact.asociatiasmart@gmail.com">
+              contact.asociatiasmart@gmail.com
+            </a>
+            .
+          </li>
+        </ul>
+
+        <p>
+          Prin utilizarea site-ului nostru, sunteți de acord cu acești Termeni
+          și Condiții.
+        </p>
+      </section>
+    </article>
+  );
 }
